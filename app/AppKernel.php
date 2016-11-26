e4b23615ac8adbccebd2a1278ca13c58b797f689
Laurent <laurent@ghirardotti.fr>
2016-11-26 18:01:40 +0800

    Adding FOS/RestBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new FOS\RestBundle\FOSRestBundle(),
+
             new AppBundle\AppBundle(),
             new CarpeDeumBundle\CarpeDeumBundle(),
         ];
