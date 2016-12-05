16cbd3becfa4a0465fc6967596e9c71247f22eab
Laurent <laurent@ghirardotti.fr>
2016-12-05 08:34:08 +0800

    Add missing Bundle in Kernel :/

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             new FOS\RestBundle\FOSRestBundle(),
+            new Sylius\Bundle\ResourceBundle\SyliusResourceBundle(),
 
             new AppBundle\AppBundle(),
             new CarpeDeumBundle\CarpeDeumBundle(),
