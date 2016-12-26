75c4f748342233534e6189bed46b524233ca4d80
Laurent <laurent@ghirardotti.fr>
2016-12-26 14:14:28 +0800

    First working test

--- src/CDBehat/FixtureContext.php
+++ src/CDBehat/FixtureContext.php
@@ -17,15 +17,34 @@ class FixtureContext extends DefaultFixtureContext
         $factory = $this->getFactory('place');
         $em = $this->getEntityManager();
         $nb = 100;
+        $now = new \DateTime();
+
+        /** @var Place $place */
+        $place = $factory->createNew();
+        $place->setCreatedAt($now);
+        $place->setUpdatedAt($now);
+        $place->setName('My test church');
+        $place->setAddress1('High Street 1st');
+        $place->setZipCode('100001');
+        $place->setCity('New York');
+        $place->setCountry('USA');
+        $place->setLongitude($this->faker->longitude);
+        $place->setLatitude($this->faker->latitude);
+
+        $em->persist($place);
 
         for ($i=0; $i<$nb; $i++) {
             /** @var Place $place */
             $place = $factory->createNew();
+            $place->setCreatedAt($now);
+            $place->setUpdatedAt($now);
             $place->setName($this->faker->name);
             $place->setAddress1($this->faker->address);
             $place->setZipCode($this->faker->postcode);
             $place->setCity($this->faker->city);
             $place->setCountry($this->faker->country);
+            $place->setLongitude($this->faker->longitude);
+            $place->setLatitude($this->faker->latitude);
 
             $em->persist($place);
         }
--- tests/features/api.feature
+++ tests/features/api.feature
@@ -5,5 +5,5 @@ Feature: API
     Given there are a bunch of places
 
   Scenario: Get list of places
-    Given I am on the villa_new page
-    Then I should see "something"
+    Given I am on the place_index page
+    Then I should see "My test church"
